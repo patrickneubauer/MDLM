2987abf9213bcc03011a3cd7ea7ac6139e96a277
Bernd Landauer <syrenio@gmail.com>
2016-04-20 22:35:44 +0200

    update MixedContent

---
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupMixedContent.java
@@ -0,0 +1,57 @@
+package at.ac.tuwien.big.xmltext;
+
+import java.security.interfaces.ECKey;
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.xtend.typesystem.emf.EcoreUtil2;
+
+public class AMEGroupMixedContent {
+
+	/*
+	 * TODO: IDEA
+	 * 1. First remove the mixed content with the EFeatureMap form the Ecore
+	 * 2. Reinsert as AnyGenericConstruct  or Introducte a new PCDATA class.
+	 * 
+	 */
+	public static void doMixedContent(Resource ecore, EcoreXSDMapper mapper){
+
+		EPackage pack = (EPackage) ecore.getAllContents().next();
+		
+		Collection<EObject> all = EcoreUtil2.allContents(pack);
+		
+		all.forEach(itm -> {
+			if(itm instanceof EClass && !((EClass) itm).getName().equals("DocumentRoot")){
+				EClass eclass = (EClass) itm;
+				System.err.println(eclass.getName());
+				EStructuralFeature foundMixed = null;
+				
+				for(EStructuralFeature esf : eclass.getEAllStructuralFeatures()){
+					System.out.println(esf.getName());
+					
+					if(esf.getName().equals("mixed")){
+						foundMixed = esf;
+					}
+				}
+				if(foundMixed!=null){
+					eclass.getEStructuralFeatures().remove(foundMixed);
+					EAnnotation anno = eclass.getEAnnotation(ExtendedMetaData.ANNOTATION_URI);
+					//anno.getDetails().remove("kind");
+					anno.getDetails().put("kind", "elementOnly");
+				}
+				
+			}
+		});
+		
+	}
+}

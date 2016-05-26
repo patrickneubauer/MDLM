dcb3ad0b1417429862168dedb96b9f89eaf6b743
Bernd Landauer <syrenio@gmail.com>
2016-05-26 22:02:16 +0200

    try alternative mixedContent construct

--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupMixedContent.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupMixedContent.java
@@ -15,6 +15,7 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
@@ -42,27 +43,20 @@ public class AMEGroupMixedContent {
 
 	private static final String TEXT_CONTENT = "textContent";
 
-	public static Map<String,EClass> storage = new HashMap<>();
-	
+	public static Map<String, EClass> storage = new HashMap<>();
+
 	private final static String ANY_GENERIC_ELEMENT = "anyGenericElement";
-	
-	
+
+	//private static MixedContentSolution solutionMethod = MixedContentSolution.ANY_GENERIC_CONSTRUCT;
+	private static MixedContentSolution solutionMethod = MixedContentSolution.INSERTING_TEXTCONTENT;
+
 	/*
-	 * TODO: IDEA
-	 * BookType returns BookType:
-		'BookType'
-		name=ID0
-		'{'
-			('text' texts+=EString)?
-			'title' title=String0
-			('text' texts+=EString)?
-			'pages' pages=Int0
-			('text' texts+=EString)?
-		'}';
+	 * TODO: IDEA BookType returns BookType: 'BookType' name=ID0 '{' ('text'
+	 * texts+=EString)? 'title' title=String0 ('text' texts+=EString)? 'pages'
+	 * pages=Int0 ('text' texts+=EString)? '}';
 	 * 
 	 */
-	
-	
+
 	/*
 	 * TODO: IDEA 1. First remove the mixed content with the EFeatureMap form
 	 * the Ecore 2. Reinsert as AnyGenericConstruct or Introducte a new PCDATA
@@ -85,187 +79,198 @@ public class AMEGroupMixedContent {
 					EStructuralFeature foundMixed = eclass.getEStructuralFeature("mixed");
 					eclass.getEStructuralFeatures().remove(foundMixed);
 					EAnnotation anno = eclass.getEAnnotation(ExtendedMetaData.ANNOTATION_URI);
-					//anno.getDetails().put("kind", "elementOnly");
+					// anno.getDetails().put("kind", "elementOnly");
 
 					// remove derived, transient, volatile from features (set
 					// when type is mixed)
 					for (EStructuralFeature esf : eclass.getEAllStructuralFeatures()) {
-						
-						//esf.getEType().eClass().getESuperTypes().add((EClass) pack.getEClassifier(AMEGroupUtil.ANY_GENERIC_ELEMENT));
+
+						// esf.getEType().eClass().getESuperTypes().add((EClass)
+						// pack.getEClassifier(AMEGroupUtil.ANY_GENERIC_ELEMENT));
 						System.out.println(esf.getName());
 						esf.setDerived(false);
 						esf.setTransient(false);
 						esf.setVolatile(false);
+						
+						EClass x = esf.eClass();
+						x.getESuperTypes().add((EClass) pack.getEClassifier(AMEGroupUtil.ANY_GENERIC_CONSTRUCT));
 					}
 
-					//createMixedElement(pack, eclass);
+					switch (AMEGroupMixedContent.solutionMethod) {
+					case ANY_GENERIC_CONSTRUCT:
+						EReference ref = EcoreFactory.eINSTANCE.createEReference();
+						ref.setName(ANY_GENERIC_ELEMENT);
+						ref.setEType(pack.getEClassifier(AMEGroupUtil.ANY_GENERIC_CONSTRUCT));
+						ref.setLowerBound(0);
+						ref.setUpperBound(-1);
+						ref.setContainment(true);
+						eclass.getEStructuralFeatures().add(ref);
+						break;
+					case INSERTING_TEXTCONTENT:
+						EAttribute attrText = EcoreFactory.eINSTANCE.createEAttribute();
+						eclass.getEStructuralFeatures().add(attrText);
+						attrText.setName(TEXT_CONTENT);
+						attrText.setEType(EcorePackage.Literals.ESTRING);
+						attrText.setLowerBound(0);
+						attrText.setUpperBound(-1);
+						break;
+					default:
+						System.err.println("MixedContent: NO VALID SOLUTION");
+						break;
+					}
 
-					//eclass.getESuperTypes().add((EClass) pack.getEClassifier(AMEGroupUtil.MIXED_CONTENT_CLASS));
-					
-					
-					
-//					EReference ref = EcoreFactory.eINSTANCE.createEReference();
-//					ref.setName(ANY_GENERIC_ELEMENT);
-//					ref.setEType(pack.getEClassifier(AMEGroupUtil.ANY_GENERIC_ELEMENT));
-//					ref.setLowerBound(0);
-//					ref.setUpperBound(-1);
-//					ref.setContainment(true);
-//					eclass.getEStructuralFeatures().add(ref);
-			
+					// createMixedElement(pack, eclass);
+					// eclass.getESuperTypes().add((EClass)
+					// pack.getEClassifier(AMEGroupUtil.MIXED_CONTENT_CLASS));
 
-					EAttribute attrText = EcoreFactory.eINSTANCE.createEAttribute();
-					eclass.getEStructuralFeatures().add(attrText);
-					attrText.setName(TEXT_CONTENT);
-					attrText.setEType(EcorePackage.Literals.ESTRING);
-					attrText.setLowerBound(0);
-					attrText.setUpperBound(-1);
-					
 					storage.put(eclass.getName(), eclass);
 				}
 			}
 		});
 
-
 	}
-	
-	private static Optional<Group> getMixedContentGroup(Group group){
-		for(EObject x : group.eContents()){
-			if(x instanceof Keyword){
-				if(((Keyword) x).getValue().equals(ANY_GENERIC_ELEMENT)){
+
+	private static Optional<Group> getMixedContentGroup(Group group) {
+		for (EObject x : group.eContents()) {
+			if (x instanceof Keyword) {
+				if (((Keyword) x).getValue().equals(ANY_GENERIC_ELEMENT)) {
 					return Optional.of((Group) x.eContainer());
 				}
 			}
-			if(x instanceof Group){
+			if (x instanceof Group) {
 				return getMixedContentGroup((Group) x);
-				
+
 			}
 		}
 		return Optional.empty();
 	}
-	
-	private static Optional<Group> getMixedContentGroup(AbstractRule rule){
-		for(EObject obj : rule.eContents()){
-			if(obj instanceof Group){
+
+	private static Optional<Group> getMixedContentGroup(AbstractRule rule) {
+		for (EObject obj : rule.eContents()) {
+			if (obj instanceof Group) {
 				Optional<Group> group = getMixedContentGroup((Group) obj);
-				if(group.isPresent())
+				if (group.isPresent())
 					return group;
 			}
 		}
 		return Optional.empty();
 	}
 
-	private static Assignment createTextAssignment(Grammar g){
+	private static Assignment createTextAssignment(Grammar g) {
 		Assignment ass = XtextFactory.eINSTANCE.createAssignment();
 		ass.setFeature(TEXT_CONTENT);
 		ass.setOperator("+=");
 		ass.setCardinality("?");
 
 		Grammar terminals = g.getUsedGrammars().get(0);
-		TerminalRule term = (TerminalRule) terminals.getRules().stream().filter(x->x.getName().equals("STRING")).findFirst().get();
+		TerminalRule term = (TerminalRule) terminals.getRules().stream().filter(x -> x.getName().equals("STRING"))
+				.findFirst().get();
 		RuleCall ruleCall = XtextFactory.eINSTANCE.createRuleCall();
 		ruleCall.setRule(term);
 		ass.setTerminal(ruleCall);
-		
+
 		return ass;
 	}
-	
-	private static Group createTextGroup(Grammar g){
+
+	private static Group createTextGroup(Grammar g) {
 		Group group = XtextFactory.eINSTANCE.createGroup();
-		//Assignment ass = XtextFactory.eINSTANCE.createAssignment();
-		//Keyword keyword = XtextFactory.eINSTANCE.createKeyword();
-		//keyword.setValue("");
-		
-		//group.getElements().add(keyword);
+		// Assignment ass = XtextFactory.eINSTANCE.createAssignment();
+		// Keyword keyword = XtextFactory.eINSTANCE.createKeyword();
+		// keyword.setValue("");
+
+		// group.getElements().add(keyword);
 		group.getElements().add(createTextAssignment(g));
 		group.setFirstSetPredicated(false);
 		group.setPredicated(false);
 		group.setCardinality("?");
-		
+
 		return group;
 	}
-	
+
 	public static void doMixedContentXText(Grammar g, EcoreXSDMapper mapper) {
 		// TODO Auto-generated method stub
-		for(Entry<String,EClass> entry: storage.entrySet()){
-			
+		for (Entry<String, EClass> entry : storage.entrySet()) {
+
 			// Find rule linked to the EClass
-			Optional<AbstractRule> absRule = g.getRules().stream().filter(x->x.getName().equals(entry.getKey())).findFirst();
-			if(absRule.isPresent()){
-				ParserRuleImpl rule = (ParserRuleImpl)absRule.get();
+			Optional<AbstractRule> absRule = g.getRules().stream().filter(x -> x.getName().equals(entry.getKey()))
+					.findFirst();
+			if (absRule.isPresent()) {
+				ParserRuleImpl rule = (ParserRuleImpl) absRule.get();
 				// what is rule.type and rule.alternatives
-				
+
 				// API XText Grammar or XText Grammar API
 				// XPand Xtend
-				
-				//Optional<Group> mixedContentGroup = getMixedContentGroup(rule);
-				
-//				if(mixedContentGroup.isPresent()){
-//					(mixedContentGroup.get()).getElements().forEach(e -> {
-//						System.out.println(e);
-//						
-//						if(e instanceof Keyword && ((Keyword) e).getValue().equals(ANY_GENERIC_ELEMENT)){
-//							((Keyword) e).setValue("mixed");
-//							
-//						}
-//					});
-//				}
-				EClass clz = (EClass) rule.getType().getClassifier();
-				List<String> ignoredFeatures = Arrays.asList(TEXT_CONTENT,"name");
-				List<String> featuresInClass =  clz.getEStructuralFeatures().stream()
-						.filter(x-> {
-							
-							return !ignoredFeatures.contains(x.getName());	
-						})
-						.map(x->x.getName()).collect(Collectors.toList());
-				System.out.println(featuresInClass);
-				
-				
-				
-				rule.eContents().forEach(itm -> {
-					if(itm instanceof Group){
-						Group grp = (Group) itm;
-						System.out.println(grp.getElements());
-						Map<Integer,Assignment> map = new TreeMap<Integer, Assignment>();
-						
-						for (int idx = 0; idx < grp.getElements().size(); idx++) {
-							AbstractElement elm = grp.getElements().get(idx);
-							if(elm instanceof Assignment){
-								if(featuresInClass.contains(((Assignment) elm).getFeature())){
-									
-									//Group insertGroup = createTextGroup(g);
-									//map.put(idx+map.size()-1, insertGroup);
-									Assignment assignment = createTextAssignment(g);
-									map.put(idx+map.size()-1, assignment);
-								}
-							}
-							if(elm instanceof Group){
-								AbstractElement e = ((Group) elm).getElements().get(0);
-								
-								Keyword k = (Keyword) e;
-								if(k.getValue().equals(TEXT_CONTENT)){
-									Assignment ass = createTextAssignment(g);
-									map.put(idx+map.size(), ass);
-								}
-								grp.getElements().remove(idx);
-							}
-						}
-						//Keyword keyword = XtextFactory.eINSTANCE.createKeyword();
-						//keyword.setValue("test");
-						//grp.getElements().add(keyword);
-						map.entrySet().forEach(x -> {
-							grp.getElements().add(x.getKey(),x.getValue());
-						});
-						
 
-						//System.out.println(insertGroup);
-					}
-				});
-								
-				System.out.println("-----");
-				System.out.println(rule.eContents());
+				// Optional<Group> mixedContentGroup =
+				// getMixedContentGroup(rule);
+
+				// if(mixedContentGroup.isPresent()){
+				// (mixedContentGroup.get()).getElements().forEach(e -> {
+				// System.out.println(e);
+				//
+				// if(e instanceof Keyword && ((Keyword)
+				// e).getValue().equals(ANY_GENERIC_ELEMENT)){
+				// ((Keyword) e).setValue("mixed");
+				//
+				// }
+				// });
+				// }
+
+				if (MixedContentSolution.INSERTING_TEXTCONTENT == AMEGroupMixedContent.solutionMethod) {
+
+					EClass clz = (EClass) rule.getType().getClassifier();
+					List<String> ignoredFeatures = Arrays.asList(TEXT_CONTENT, "name");
+					List<String> featuresInClass = clz.getEStructuralFeatures().stream().filter(x -> {
+
+						return !ignoredFeatures.contains(x.getName());
+					}).map(x -> x.getName()).collect(Collectors.toList());
+					System.out.println(featuresInClass);
+
+					rule.eContents().forEach(itm -> {
+						if (itm instanceof Group) {
+							Group grp = (Group) itm;
+							System.out.println(grp.getElements());
+							Map<Integer, Assignment> map = new TreeMap<Integer, Assignment>();
+
+							for (int idx = 0; idx < grp.getElements().size(); idx++) {
+								AbstractElement elm = grp.getElements().get(idx);
+								if (elm instanceof Assignment) {
+									if (featuresInClass.contains(((Assignment) elm).getFeature())) {
+
+										// Group insertGroup =
+										// createTextGroup(g);
+										// map.put(idx+map.size()-1,
+										// insertGroup);
+										Assignment assignment = createTextAssignment(g);
+										map.put(idx + map.size() - 1, assignment);
+									}
+								}
+								if (elm instanceof Group) {
+									AbstractElement e = ((Group) elm).getElements().get(0);
+
+									Keyword k = (Keyword) e;
+									if (k.getValue().equals(TEXT_CONTENT)) {
+										Assignment ass = createTextAssignment(g);
+										map.put(idx + map.size(), ass);
+									}
+									grp.getElements().remove(idx);
+								}
+							}
+							map.entrySet().forEach(x -> {
+								grp.getElements().add(x.getKey(), x.getValue());
+							});
+						}
+					});
+
+					System.out.println("-----");
+					System.out.println(rule.eContents());
+				}
 			}
-				
-			
+
 		}
+	}
+
+	private enum MixedContentSolution {
+		INSERTING_TEXTCONTENT, ANY_GENERIC_CONSTRUCT
 	}
 }
--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupUtil.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupUtil.java
@@ -31,55 +31,92 @@ public class AMEGroupUtil {
 			return;
 		
 		// AnyGenericConstruct
-		EClass anyGConstruct = EcoreFactory.eINSTANCE.createEClass();
-		anyGConstruct.setName(ANY_GENERIC_CONSTRUCT);
-		anyGConstruct.setAbstract(true);
-		anyGConstruct.setInterface(true);
-		pack.getEClassifiers().add(anyGConstruct);
+		EClass anyGConstruct = createConstruct(pack);
 		
 		// AnyGenericText
+		createText(pack, anyGConstruct);
+		
+		// AnyGenericAttribute
+		EClass anyGAttribute = createAttribute(pack);
+		
+		// AnyGenericElement
+		createElement(pack, anyGConstruct, anyGAttribute);
+	}
+
+	private static void createElement(EPackage pack, EClass anyGConstruct, EClass anyGAttribute) {
+		EClass anyGElement = EcoreFactory.eINSTANCE.createEClass();
+		anyGElement.setName(ANY_GENERIC_ELEMENT);
+		
+		EReference childElem = EcoreFactory.eINSTANCE.createEReference();
+		anyGElement.getEStructuralFeatures().add(childElem);
+		childElem.setName("childElem");
+		childElem.setContainment(true);
+		childElem.setEType(anyGElement);
+		childElem.setLowerBound(0);
+		childElem.setUpperBound(-1);
+		
+		EReference anyGenericAttr = EcoreFactory.eINSTANCE.createEReference();
+		anyGElement.getEStructuralFeatures().add(anyGenericAttr);
+		anyGenericAttr.setName("anyGenericAttr");
+		anyGenericAttr.setContainment(true);
+		anyGenericAttr.setEType(anyGAttribute);
+		anyGenericAttr.setLowerBound(0);
+		anyGenericAttr.setUpperBound(-1);
+		
+		EAttribute elemName = EcoreFactory.eINSTANCE.createEAttribute();
+		anyGElement.getEStructuralFeatures().add(elemName);
+		elemName.setName("elemName");
+		elemName.setLowerBound(1);
+		elemName.setUpperBound(1);
+		elemName.setEType(EcorePackage.Literals.ESTRING);
+		EAttribute elemValue = EcoreFactory.eINSTANCE.createEAttribute();
+		anyGElement.getEStructuralFeatures().add(elemValue);
+		elemValue.setName("elemValue");
+		elemValue.setEType(EcorePackage.Literals.ESTRING);
+		
+		anyGElement.getESuperTypes().add(anyGConstruct);
+		pack.getEClassifiers().add(anyGElement);
+	}
+
+	private static EClass createAttribute(EPackage pack) {
+		EClass anyGAttribute = EcoreFactory.eINSTANCE.createEClass();
+		anyGAttribute.setName(ANY_GENERIC_ATTRIBUTE);
+		EAttribute attrName = EcoreFactory.eINSTANCE.createEAttribute();
+		anyGAttribute.getEStructuralFeatures().add(attrName);
+		attrName.setName("attrName");
+		attrName.setLowerBound(1);
+		attrName.setUpperBound(1);
+		attrName.setEType(EcorePackage.Literals.ESTRING);
+		EAttribute attrValue = EcoreFactory.eINSTANCE.createEAttribute();
+		anyGAttribute.getEStructuralFeatures().add(attrValue);
+		attrValue.setName("attrValue");
+		attrValue.setLowerBound(1);
+		attrValue.setUpperBound(1);
+		attrValue.setEType(EcorePackage.Literals.ESTRING);
+		pack.getEClassifiers().add(anyGAttribute);
+		return anyGAttribute;
+	}
+
+	private static void createText(EPackage pack, EClass anyGConstruct) {
 		EClass anyGText = EcoreFactory.eINSTANCE.createEClass();
 		anyGText.setName(ANY_GENERIC_TEXT);
 		EAttribute attrText = EcoreFactory.eINSTANCE.createEAttribute();
 		anyGText.getEStructuralFeatures().add(attrText);
-		attrText.setName("text");
+		attrText.setName("textValue");
 		attrText.setEType(EcorePackage.Literals.ESTRING);
 		attrText.setLowerBound(0);
 		attrText.setUpperBound(1);
 		anyGText.getESuperTypes().add(anyGConstruct);
 		pack.getEClassifiers().add(anyGText);
-		
-		// AnyGenericAttribute
-		EClass anyGAttribute = EcoreFactory.eINSTANCE.createEClass();
-		anyGAttribute.setName(ANY_GENERIC_ATTRIBUTE);
-		pack.getEClassifiers().add(anyGAttribute);
-		
-		// AnyGenericElement
-		EClass anyGElement = EcoreFactory.eINSTANCE.createEClass();
-		anyGElement.setName(ANY_GENERIC_ELEMENT);
-		EReference childElements = EcoreFactory.eINSTANCE.createEReference();
-		anyGElement.getEStructuralFeatures().add(childElements);
-		childElements.setName("childElements");
-		childElements.setContainment(true);
-		childElements.setEType(anyGElement);
-		childElements.setLowerBound(0);
-		childElements.setUpperBound(-1);
-		EReference attr = EcoreFactory.eINSTANCE.createEReference();
-		anyGElement.getEStructuralFeatures().add(attr);
-		attr.setName("anyGenericAttr");
-		attr.setContainment(true);
-		attr.setEType(anyGAttribute);
-		attr.setLowerBound(0);
-		attr.setUpperBound(-1);
-		
-		EAttribute attrElement = EcoreFactory.eINSTANCE.createEAttribute();
-		anyGElement.getEStructuralFeatures().add(attrElement);
-		attrElement.setName("element");
-		attrElement.setEType(EcorePackage.Literals.ESTRING);
-		
-		
-		anyGElement.getESuperTypes().add(anyGConstruct);
-		pack.getEClassifiers().add(anyGElement);
+	}
+
+	private static EClass createConstruct(EPackage pack) {
+		EClass anyGConstruct = EcoreFactory.eINSTANCE.createEClass();
+		anyGConstruct.setName(ANY_GENERIC_CONSTRUCT);
+		anyGConstruct.setAbstract(true);
+		anyGConstruct.setInterface(true);
+		pack.getEClassifiers().add(anyGConstruct);
+		return anyGConstruct;
 	}
 	
 	

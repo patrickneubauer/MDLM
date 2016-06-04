7721e703f03f78195538d36c24990aa6012b326f
agnesfroeschl <agnes.froeschl@gmx.at>
2016-06-04 17:17:56 +0200

    update restrictions, transformation of annotations length, maxl and minl

--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupRestriction.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupRestriction.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.big.xmltext;
 
 import java.util.Collection;
 
+import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
@@ -18,15 +19,36 @@ public class AMEGroupRestriction {
 		Collection<EObject> all = EcoreUtil2.allContents(pack);
 		all.forEach(itm -> {
 			if (itm instanceof EDataTypeImpl) {
+
 				EDataTypeImpl edataType = (EDataTypeImpl) itm;
 				EAnnotation anno = edataType.getEAnnotation(ExtendedMetaData.ANNOTATION_URI);
-				if(edataType.getName().equals("NameType")){
-					anno.getDetails().put("NameTypeMaxLength", "self.nameType < 32");
-				}
-				System.out.println(edataType.getName());
+				EMap<String, String> details = anno.getDetails();
 
+				if (details.get("maxLength") != null) {
+					transformAnnotation("maxLength", details, anno);
+				}
+
+				if (details.get("minLength") != null) {
+					transformAnnotation("minLength", details, anno);
+				}
+
+				if (details.get("length") != null) {
+					transformAnnotation("length", details, anno);
+				}
 			}
 		});
+	}
+
+	private static void transformAnnotation(String value, EMap<String, String> details, EAnnotation anno) {
+		String maxLength = details.get(value);
+		details.remove(value);
+		anno.getDetails().put(value, fixLength(maxLength));
+	}
+
+	private static String fixLength(String oldValue) {
+		int value = Integer.parseInt(oldValue);
+		value += 2;
+		return String.valueOf(value);
 	}
 
 	public static void doRestrictionXText(Grammar g, EcoreXSDMapper mapper) {

93f650dc89dbfe2eb8cb8cbf5df452647efa2e27
Bernhard Müller <bernhard.mueller@gmx.at>
2016-06-13 03:13:57 +0200

    extended XText refaction

--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupIdIdref.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupIdIdref.java
@@ -1,6 +1,8 @@
 package at.ac.tuwien.big.xmltext;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EAttribute;
@@ -10,23 +12,27 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.impl.EAttributeImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xml.type.XMLTypePackage;
 import org.eclipse.xtend.typesystem.emf.EcoreUtil2;
 import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.Grammar;
-import org.eclipse.xtext.impl.RuleCallImpl;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.TypeRef;
+import org.eclipse.xtext.XtextFactory;
+import org.eclipse.xtext.impl.KeywordImpl;
+import org.eclipse.xtext.impl.TypeRefImpl;
 
 public class AMEGroupIdIdref {
 	
 	// Name of Common ID EClass
-	private final static String ECLASS_ID_NAME = "IdValues";
+	private final static String ECLASS_COMMON_ID_NAME = "IdValues";
 	// EStructuralFeature Attribute Name of Common ID EClass
-	private final static String ECLASS_ID_ATTRIBUTE_NAME = "name";
+	private final static String ECLASS_COMMON_ID_ATTRIBUTE_NAME = "name";
 	
 	public static void implIdIdref(Resource ecore, EcoreXSDMapper mapper) {
-		// TODO Test with multiple IDs
 		refactorIdIdref(ecore);
 	}
 
@@ -37,31 +43,31 @@ public class AMEGroupIdIdref {
 	
 	/**
 	 * Create common EClass for ID and IDREF. Reference all ID elements by containment and all IDREF elements by non-containment EReference.
-	 * @param ecore
+	 * @param ecore Resource
 	 */
 	private static void refactorIdIdref(Resource ecore) {
 
 		
 		EPackage pack = (EPackage) ecore.getAllContents().next();
-				
+		String eclassCommonIdName = findFreeEClassifier(pack, ECLASS_COMMON_ID_NAME, 0);
+		
 		//create the common ID-EClass and add to Ecore
-		//TODO test if name exists
 		EAttribute esa = EcoreFactory.eINSTANCE.createEAttribute();
-			esa.setName(ECLASS_ID_ATTRIBUTE_NAME);
+			esa.setName(ECLASS_COMMON_ID_ATTRIBUTE_NAME);
 			esa.setEType(XMLTypePackage.Literals.ID);
 			esa.setUpperBound(1);
 			esa.setID(true);
-		EClass eClassIdValues = EcoreFactory.eINSTANCE.createEClass();
-			eClassIdValues.setName(ECLASS_ID_NAME);
-			eClassIdValues.getEStructuralFeatures().add(esa);
-		pack.getEClassifiers().add(eClassIdValues);
+		EClass eclassCommonId = EcoreFactory.eINSTANCE.createEClass();
+			eclassCommonId.setName(eclassCommonIdName);
+			eclassCommonId.getEStructuralFeatures().add(esa);
+		pack.getEClassifiers().add(eclassCommonId);
 		
 
 		//search all ID or IDREF EStructuralFeatures and replace with EReference to eClassIdValues
 		Collection<EObject> all = EcoreUtil2.allContents(pack);
 
 		for(EObject itm : all) {
-			if (itm instanceof EClass && ((EClass) itm).getName() != ECLASS_ID_NAME) {
+			if (itm instanceof EClass && ((EClass) itm).getName() != eclassCommonIdName) {
 				EClass eclass = (EClass) itm;
 				
 				for (EStructuralFeature esf : eclass.getEAllStructuralFeatures()) {
@@ -71,10 +77,11 @@ public class AMEGroupIdIdref {
 						EReference erf = EcoreFactory.eINSTANCE.createEReference();
 
 						AMEGroupUtil.copyEStructuralFeature(esf, erf);
-						erf.setContainment(true);
-						erf.setEType(eClassIdValues);					
 						
-						AMEGroupUtil.replaceEStructuralFeature(eclass, esf, erf);
+						erf.setContainment(true);
+						erf.setEType(eclassCommonId);					
+						
+						EcoreUtil.replace(esf, erf);
 				
 					} else if (esf.getEType().equals(XMLTypePackage.Literals.IDREF)) {
 						
@@ -83,9 +90,9 @@ public class AMEGroupIdIdref {
 						AMEGroupUtil.copyEStructuralFeature(esf, erf);
 						
 						erf.setContainment(false);
-						erf.setEType(eClassIdValues);
+						erf.setEType(eclassCommonId);
 						
-						AMEGroupUtil.replaceEStructuralFeature(eclass, esf, erf);
+						EcoreUtil.replace(esf, erf);
 					
 					}
 				}
@@ -95,47 +102,101 @@ public class AMEGroupIdIdref {
 	
 
 	/**
-	 * Removes RuleCalls to null.
-	 * Ecore2Xtext creates [IdValues|EString] for IDREF. EString is not valid for IDREF and furthermore undefined and has to be removed.
-	 * @param g
-	 * @param mapper
+	 * Removes TypeRef/RullCalles except to Common ID EClass TypeRef, for IDREF CrossReferences.
+	 * 
+	 * Ecore2Xtext creates [IdValues|EString] for EReference [containment = false] (xs:IDREF). 
+	 * EString is not valid for IDREF and furthermore undefined, so it has to be removed.
+	 * @param g XText Grammar
+	 * @param mapper EcoreXSDMapper
 	 */
 	private static void refactorIdIdrefXText(Grammar g, EcoreXSDMapper mapper) {
 		
 
-//		EPackage pack = mapper.getEcorePackage();
-
+		List<String> eclassCommonIdName = new ArrayList<String>();
 		
-		// iterate over all CrossReferences and remove RullCales to null
+		// iterate over all CrossReferences and remove TypeRef/RullCalles except to Common ID EClass TypeRef
 		g.getRules().forEach(rule -> {
 			TreeIterator<Object> allContents = EcoreUtil.getAllContents(rule.getAlternatives(), true);
 			allContents.forEachRemaining(elm -> {
 				if(elm instanceof CrossReference){
-					CrossReference ass = (CrossReference) elm;
+					CrossReference crossRef = (CrossReference) elm;
+					EList<EObject> crossRefContents = crossRef.eContents();
+					List<EObject> crossRefContentsToRemove = new ArrayList<EObject>();
+					List<Boolean> isIdCrossRef = new ArrayList<Boolean>();
 					
+					crossRefContents.forEach(crossRefContent -> {
 
-					
-					
-					
-					EList<EObject> a = ass.eContents();
-					
-					a.forEach(e -> {
-//						System.err.println(e.getClass());
-						
-						if(e.getClass() == RuleCallImpl.class) {
-							RuleCallImpl t = (RuleCallImpl) e;
-							if (t.getRule() == null) {
-			//TODO only remove if CrossReference contains TypeRef to IdValues EClass (has eType XMLTypePackage.Literals.ID)
-								EcoreUtil.remove(t);
-							}
+						// if Content is TypeRef to Common ID EClass
+						if(crossRefContent.getClass() == TypeRefImpl.class) {
+
+							((TypeRef) crossRefContent).getClassifier().eContents().forEach(esf -> {
+								
+								//TODO useless if only ID but no IDREF available
+								eclassCommonIdName.add(((TypeRef) crossRefContent).getClassifier().getName());
+								
+								// if EStructuralFeature is an EAttribute and has eType XML ID -> it is the 
+								if (esf.getClass().equals(EAttributeImpl.class) && ((EAttribute) esf).getEType().equals(XMLTypePackage.Literals.ID)) {
+									isIdCrossRef.add(true);
+									/* Alternative:
+									 * //scan all elements of CrossReference again and remove RuleCalls to null
+										crossRefContents.forEach(crc -> {
+											if(crc.getClass() == RuleCallImpl.class) {
+												RuleCall rc = (RuleCall) crc;
+												if (rc.getRule() == null) {
+													EcoreUtil.remove(rc);												
+												}
+											}
+										});*/
+								}
+							});
+
 						}
-
+						else {
+							// add CrossReferenceContents != Common ID EClass TypeRef
+							crossRefContentsToRemove.add(crossRefContent);
+						}
+					});
+					
+					// remove all 
+					if(!isIdCrossRef.isEmpty()) {
+						crossRefContentsToRemove.forEach(crctr -> {
+							EcoreUtil.remove(crctr);
+						});
+					}
+				}
+			});
+		});
+		
+		// remove the extra keyword in common ID rule
+		if (!eclassCommonIdName.isEmpty()) {
+			g.getRules().forEach(rule -> {
+				if (rule.getName().equals(eclassCommonIdName.get(0))) {
+					TreeIterator<Object> allContents = EcoreUtil.getAllContents(rule.getAlternatives(), true);
+					allContents.forEachRemaining(elm -> {
+						if (elm.getClass().equals(KeywordImpl.class)) {
+							Keyword kw = (Keyword) elm;
+							org.eclipse.xtext.EcoreUtil2.remove(kw);
+						}
 					});
 				}
 			});
+		}
+		
+		
+		
+	}
+	
+	
+	private static String findFreeEClassifier(EPackage pack, String eclassIdName, int counter) {
+		
+		while (pack.getEClassifier(eclassIdName) != null) {
 			
+			counter++;
+			eclassIdName = eclassIdName + counter;
 			
-		});
+		}
+		
+		return eclassIdName;
 		
 	}
 }

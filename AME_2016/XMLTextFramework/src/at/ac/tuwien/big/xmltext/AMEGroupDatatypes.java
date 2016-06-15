574405fcae5110cac3a5b5262ab0351e266ebcfc
agnesfroeschl <agnes.froeschl@gmx.at>
2016-06-15 22:29:17 +0200

    transformation of types "javax.xml.datatype.XMLGregorianCalendar"

--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupDatatypes.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupDatatypes.java
@@ -1,7 +1,25 @@
 package at.ac.tuwien.big.xmltext;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.antlr.runtime.ParserRuleReturnScope;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.EDataTypeImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtend.typesystem.emf.EDataTypeType;
+import org.eclipse.xtext.AbstractMetamodelDeclaration;
 import org.eclipse.xtext.AbstractRule;
 import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.ParserRule;
@@ -9,52 +27,57 @@ import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.TypeRef;
 import org.eclipse.xtext.XtextFactory;
+import org.eclipse.xtext.common.types.util.TypeReferences;
+import org.eclipse.xtext.generator.parser.antlr.debug.services.SimpleAntlrGrammarAccess.AlternativesElements;
+import org.eclipse.xtext.generator.parser.antlr.debug.simpleAntlr.impl.AlternativesImpl;
+import org.eclipse.xtext.generator.parser.antlr.debug.simpleAntlr.impl.RuleCallImpl;
+import org.eclipse.xtext.impl.AbstractElementImpl;
+import org.eclipse.xtext.impl.ParserRuleImpl;
+import org.eclipse.xtext.impl.TypeRefImpl;
+import org.eclipse.xtext.impl.XtextFactoryImpl;
 
 public class AMEGroupDatatypes {
 
 	public static void implDatatypesXText(Grammar g, EcoreXSDMapper mapper) {
 		List<AbstractRule> rules = g.getRules();
 		for (AbstractRule r : rules) {
-			if (r.getName().equals("String0")) {
-				r.setAlternatives(callTerminal(g, "STRING"));
+			String instanceTypeName = r.getType().getClassifier().getInstanceTypeName();
+			if (instanceTypeName == null) {
+				continue;
 			}
-			else if (r.getName().equals("ID0")) {
-				r.setAlternatives(callTerminal(g, "ID"));
-			}
-			else if (r.getName().equals("Int0")) {
+			
+			switch (instanceTypeName) {
+			case "java.math.BigInteger":
 				r.setAlternatives(callTerminal(g, "INT"));
+				break;
+			case "int": {
+				r.setAlternatives(callTerminal(g, "INT"));
+				break;
 			}
-			else{
-				String instanceTypeName = r.getType().getClassifier().getInstanceTypeName();
-				if(instanceTypeName == null){
-					continue;
-				}
-				ParserRule pr;
-				TypeRef ref;
-				switch (instanceTypeName) {
-				case "java.math.BigInteger":
-					r.setAlternatives(callTerminal(g, "INT"));
-					/*
-					ref = XtextFactory.eINSTANCE.createTypeRef();
-					ref.setClassifier(EcorePackage.Literals.EBIG_INTEGER);
-					pr.setType(ref);
-					*/
-					break;
-				case "int":{
-					r.setAlternatives(callTerminal(g, "INT"));
-					break;
-				}
-				case "java.lang.String":{
-					r.setAlternatives(callTerminal(g, "STRING"));
-					break;
-				}
-				default:
-					System.out.println(instanceTypeName);
-					break;
-				}
+			case "java.lang.String": {
+				r.setAlternatives(callTerminal(g, "STRING"));
+				break;
+			}
+			case "javax.xml.datatype.XMLGregorianCalendar": {
+				r.setAlternatives(callTerminal(g, "STRING"));
+				break;
+			}
+			default:
+				System.out.println(instanceTypeName);
+				break;
 			}
 		}
+	}
 
+	/*smaller example for paper*/
+	public static void implDatatypesXText(Grammar g) {
+		List<AbstractRule> rules = g.getRules();
+		for (AbstractRule r : rules) {
+			String instanceTypeName = r.getType().getClassifier().getInstanceTypeName();
+			if (instanceTypeName.equals("java.lang.String")) {
+				r.setAlternatives(callTerminal(g, "STRING"));
+			}
+		}
 	}
 
 	private static RuleCall callTerminal(Grammar g, String name) {

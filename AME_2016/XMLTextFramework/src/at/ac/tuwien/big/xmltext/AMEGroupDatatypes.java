b57262d64358bcaaa920069b7183b26fc70b9489
agnesfroeschl <agnes.froeschl@gmx.at>
2016-06-29 22:20:10 +0200

    add some more datatypes: boolean, decimal, time ..

--- AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupDatatypes.java
+++ AME_2016/XMLTextFramework/src/at/ac/tuwien/big/xmltext/AMEGroupDatatypes.java
@@ -1,44 +1,17 @@
 package at.ac.tuwien.big.xmltext;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.antlr.runtime.ParserRuleReturnScope;
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EClassifier;
-import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.impl.EDataTypeImpl;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtend.typesystem.emf.EDataTypeType;
 import org.eclipse.xtext.AbstractElement;
-import org.eclipse.xtext.AbstractMetamodelDeclaration;
 import org.eclipse.xtext.AbstractRule;
 import org.eclipse.xtext.Alternatives;
 import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.Group;
 import org.eclipse.xtext.Keyword;
-import org.eclipse.xtext.ParserRule;
 import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.TerminalRule;
-import org.eclipse.xtext.TypeRef;
 import org.eclipse.xtext.XtextFactory;
-import org.eclipse.xtext.common.types.util.TypeReferences;
-import org.eclipse.xtext.generator.parser.antlr.debug.services.SimpleAntlrGrammarAccess.AlternativesElements;
-import org.eclipse.xtext.generator.parser.antlr.debug.simpleAntlr.impl.AlternativesImpl;
-import org.eclipse.xtext.generator.parser.antlr.debug.simpleAntlr.impl.RuleCallImpl;
-import org.eclipse.xtext.impl.AbstractElementImpl;
-import org.eclipse.xtext.impl.ParserRuleImpl;
-import org.eclipse.xtext.impl.TypeRefImpl;
-import org.eclipse.xtext.impl.XtextFactoryImpl;
+
 
 public class AMEGroupDatatypes {
 
@@ -46,28 +19,50 @@ public class AMEGroupDatatypes {
 		List<AbstractRule> rules = g.getRules();
 		for (AbstractRule r : rules) {
 			String instanceTypeName = r.getType().getClassifier().getInstanceTypeName();
-			if (instanceTypeName == null) {
+			String classifierName = r.getType().getClassifier().getName();
+			if (classifierName == null) {
 				continue;
 			}
 
-			switch (instanceTypeName) {
+			switch (classifierName) {
 			case "java.math.BigInteger":
 				r.setAlternatives(callTerminal(g, "INT"));
 				break;
-			case "int": {
+			case "Decimal": {
+				r.setAlternatives(createDecimalGroup(g));
+				break;
+			}
+			case "Integer": {
 				r.setAlternatives(callTerminal(g, "INT"));
 				break;
 			}
-			case "java.lang.String": {
+			case "String": {
 				r.setAlternatives(callTerminal(g, "STRING"));
 				break;
 			}
-			case "javax.xml.datatype.XMLGregorianCalendar": {
+			case "EString": {
+				r.setAlternatives(callTerminal(g, "STRING"));
+				break;
+			}
+			case "Date": {
 				r.setAlternatives(createDateGroup(g));
 				break;
 			}
+			case "Time": {
+				r.setAlternatives(createTimeGroup(g));
+				break;
+			}
+			/*case "DateTime": {
+				r.setAlternatives(createDateTimeGroup(g));
+				break;
+			}*/
+			case "Boolean": {
+				r.setAlternatives(createBooleanRule(g));
+				break;
+			}
 			default:
-				System.out.println(instanceTypeName);
+				System.out.println("instanceTypeName: "+instanceTypeName);
+				System.out.println("classifier: "+classifierName);
 				break;
 			}
 
@@ -89,10 +84,55 @@ public class AMEGroupDatatypes {
 		return group;
 	}
 	
+	private static Group createTimeGroup(Grammar g) {
+		Group group = XtextFactory.eINSTANCE.createGroup();
+		List<AbstractElement> groupElements = group.getElements();
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword(":"));
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword(":"));
+		groupElements.add(callTerminal(g, "INT"));
+		return group;
+	}
+	
+	private static Group createDateTimeGroup(Grammar g) {
+		Group group = XtextFactory.eINSTANCE.createGroup();
+		List<AbstractElement> groupElements = group.getElements();
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword("-"));
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword("-"));
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword("T"));
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword(":"));
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword(":"));
+		groupElements.add(callTerminal(g, "INT"));
+		return group;
+	}
+	
+	private static Group createDecimalGroup(Grammar g) {
+		Group group = XtextFactory.eINSTANCE.createGroup();
+		List<AbstractElement> groupElements = group.getElements();
+		groupElements.add(callTerminal(g, "INT"));
+		groupElements.add(createKeyword("."));
+		groupElements.add(callTerminal(g, "INT"));
+		return group;
+	}
+	
 	private static Keyword createKeyword(String s){
 		Keyword keyword = XtextFactory.eINSTANCE.createKeyword();
 		keyword.setValue(s);
 		return keyword;
+	}
+	
+	private static Alternatives createBooleanRule(Grammar g){
+		Alternatives booleanAl = XtextFactory.eINSTANCE.createAlternatives();
+		List<AbstractElement> alternativesEl = booleanAl.getElements();
+		alternativesEl.add(createKeyword("true"));
+		alternativesEl.add(createKeyword("false"));
+		return booleanAl;
 	}
 	
 
